2e045960febf3e32a4dda68a010ccc8edca396c6
ddio <ddioibb@gmail.com>
2018-05-31 22:50:54 +0800

    Init a nuxt empty project

---
+++ web/ui/nuxt.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: '開租',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '開放台灣民間租屋資料' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
