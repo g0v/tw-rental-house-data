5cb88366d3a855668bdac4112e6ab1c1ab640b0f
ddio <ddioibb@gmail.com>
2020-01-19 15:09:11 +0800

    Add 2019 Dec, Q4, and annual data

--- ui/components/AnnualDownload.vue
+++ ui/components/AnnualDownload.vue
@@ -9,7 +9,7 @@
     .annual__seg.seg(v-if="definition.annual.length")
       .seg__title 年度資料
       .seg__table
-        DownloadTable(:rows="definition.annual")
+        DownloadTable(idHeader="年度", :rows="definition.annual")
     .annual__seg.seg(v-if="definition.quarterly.length")
       .seg__title 每季資料
       .seg__table
