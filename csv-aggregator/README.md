50cb01f8bb66635d0575c5bf9a946e97d1e6f338
ddio <ddioibb@gmail.com>
2023-12-03 23:01:55 +0800

    feat: add pack helper

---
+++ csv-aggregator/README.md
@@ -0,0 +1,35 @@
+# 資料合併、驗證小幫手
+
+## 環境需求
+
+1. [Clickhouse local](https://clickhouse.com/docs/en/operations/utilities/clickhouse-local)
+
+## 合併小幫手
+
+小幫手將使用 Clickhouse local ，將多個單月的原始 CSV ，合併為跨月的原始 +
+去除重複資料的資料集。自 2023 開始，本資料集的季度、年度資料，也是使用這支程式製作。
+
+準備工作：
+
+1. 將要合併的所有 `zip` ，例如 `[YYYYMM][CSV][Raw] TW-Rental-Data.zip`，放到同一個資料夾中，比如說 `source/`
+2. 確定要產稱的檔案名稱前綴，例如 2025Q1
+
+```bash
+./pack.sh <source dir> <YYYYOO>
+```
+
+以 `YYYYOO` = `2025Q1` 為例，執行完畢後，就可在資料夾中看到：
+
+1. `[2025Q1][CSV][Raw] TW-Rental-Data.zip`
+2. `[2025Q1][CSV][Deduplicated] TW-Rental-Data.zip`
+
+## 驗證小幫手
+
+計算單月資料集中，CSV 、JSON 物件數量差異，並匯入編碼表的小幫手。
+
+使用前，請先確定下載的 `.zip` ，和 `check.sh` 在同一個資料夾中。
+
+```bash
+check.sh <YYYYMM>
+```
+
---
+++ csv-aggregator/pack.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -e
+
+# change to the directory that contains this script
+cd "$(dirname "$0")"
+
+# directory that contains all raw zip files, say, [202501][CSV][Raw] TW-Rental-Data.zip
+sourceDir=$1
+
+# result name, say, 2025Q1, the result will be
+# 1. [2025Q1][CSV][Raw] TW-Rental-Data.zip
+# 2. [2025Q1][CSV][[Deduplicated] TW-Rental-Data.zip
+targetName=$2
+
+# exit if sourceDir or targetName is not specified
+if [ -z "$sourceDir" ] || [ -z "$targetName" ]; then
+  echo "Usage: $0 <sourceDir> <targetName>"
+  exit 1
+fi
+
+# unzip files to a temp directory
+tempDir=`mktemp -d`
+
+# remove tempDir at the end
+trap "rm -rf $tempDir" EXIT
+
+# file in zip may contain space
+mkdir $tempDir/raw $tempDir/result $tempDir/tw-rental-data
+IFS=$'\n'
+for file in `ls $sourceDir/*.zip`; do
+  unzip "$file" -d $tempDir/raw
+  # move aaa dir to $tempDir/raw if not existed
+  if [ ! -d "$tempDir/tw-rental-data/編碼表" ]; then
+    mv $tempDir/raw/tw-rental-data/編碼表 $tempDir/tw-rental-data
+  else
+    rm -rf $tempDir/raw/tw-rental-data/編碼表
+  fi
+done
+
+mv $tempDir/raw/tw-rental-data/*-raw.csv $tempDir/raw
+
+cp merge.sql $tempDir
+
+echo "Merge files..."
+(cd $tempDir; clickhouse local --queries-file merge.sql)
+
+mv $tempDir/result/raw.csv "$tempDir/tw-rental-data/${targetName}-raw.csv"
+(cd $tempDir; zip -r "[$targetName][CSV][Raw] TW-Rental-Data.zip" tw-rental-data; mv tw-rental-data/*.csv ./result)
+
+mv $tempDir/result/deduplicated.csv "$tempDir/tw-rental-data/$targetName-deduplicated.csv"
+(cd $tempDir; zip -r "[$targetName][CSV][Deduplicated] TW-Rental-Data.zip" tw-rental-data/)
+
+echo "Result:"
+(cd $tempDir; ls -lh *.zip)
+
+mv $tempDir/*.zip .
+
+
