4bd496d7bdc697eb3cec060fee100e0c95150be0
ddio <ddio@users.noreply.github.com>
2024-11-09 07:52:03 +0000

    deploy: b54ccce46049912779b4562ac80fe6a8f8c8a100

---
+++ _nuxt/static/1731138717/download/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1731138717",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdownload",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4e12925d"}}}}(null));
---
+++ _nuxt/static/1731138717/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1731138717",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4e12925d"}}}}(null,"\u002F"));
