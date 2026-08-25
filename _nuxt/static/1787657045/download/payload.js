2d8d3b912b856b4a89eaf20fd5a8a34cff1fbd54
ddio <ddio@users.noreply.github.com>
2026-08-25 11:48:14 +0000

    deploy: 5fb108e620b12c06e6c2f426cf021e47b3aaae25

--- _nuxt/static/1787657045/download/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/download", {data:[{}],fetch:{},mutations:void 0});
--- _nuxt/static/1787657045/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1787657045",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"71d825ee"}}}}(null,"\u002F"));
